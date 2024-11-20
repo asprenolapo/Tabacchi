165ce657256ec4481f0fdce958abb90725c7ac1d
asprenolapo <aslape.lapo@gmail.com>
2024-11-20 19:33:56 +0100

    commenti, main, section2 e footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,17 +7,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="shortcut icon" href="https://thumbs.dreamstime.com/b/sigaro-17258998.jpg" type="">
-    <title>{{$titlePage ?? 'Tabaccheria 195'}}</title>
+    <link rel="shortcut icon" href="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/11357.png" type="">
+    <title>
+        {{$titlePage ?? 'Tabaccheria 195'}}
+    </title>
 </head>
 
 <body>
+
     <x-navbar />
+
     <div class="min-vh-100">
         {{ $slot }}
     </div>
+
     <x-footer />
+
     <script src="https://kit.fontawesome.com/7d149bc2d8.js" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,56 @@
 <x-layout>
+    {{-- HEADER --}}
     <header class="container-fluid p-0 overflow-hidden">
         <div class="card text-bg-dark overflow-hidden">
-            <img src="https://www.lastanzadelte.it/wp-content/uploads/2012/02/sigaro.jpg" class="card-img header-img" alt="...">
+            <img src="https://www.lastanzadelte.it/wp-content/uploads/2012/02/sigaro.jpg" class="card-img header-img"
+                alt="...">
             <div class="card-img-overlay d-flex flex-column justify-content-center">
-                <h5 class="card-title display-4" >Tabaccheria 195</h5>
-                <p class="card-text fs-5">Tabaccai dal 1969 adibiti a vendita di sigari</p>
+                <h5 class="card-title display-4">Tabaccheria 195</h5>
+                <p class="card-text fs-5">Tabaccai dal 1985 adibiti a vendita di sigari</p>
                 <p class="card-text"><small>Last updated 3 mins ago</small></p>
             </div>
         </div>
     </header>
+    {{-- /HEADER --}}
+
+    {{-- SECTION I --}}
+    <section class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 text-center my-5">
+                <h3 class="text-danger">Chi siamo</h3>
+                <h2>Tabaccheria 195</h2>
+                <img src="/" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia error aperiam corrupti commodi
+                    quam? Officia impedit neque ducimus recusandae iusto repudiandae.</p>
+                <p>Leonardo - Owner</p>
+            </div>
+        </div>
+    </section>
+    {{-- /SECTION I --}}
+
+    {{-- MAIN --}}
+    <main>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 text-center my-5">
+                <h3 class="text-danger">Best Sellers</h3>
+                <p>I più venduti i più apprezzati</p>
+            </div>
+        </div>
+    </main>
+    {{-- /MAIN --}}
+
+    {{-- SECTION II --}}
+    <section class="container-fluid p-0 overflow-hidden">
+        <div class="card text-bg-dark overflow-hidden">
+            <img class="section2-img" src="https://images.unsplash.com/photo-1547424450-a69b33b2cdc2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"alt="...">
+            <div class="card-img-overlay d-flex flex-column justify-content-center text-center">
+                <h5 class="card-title display-4">Luxury Cigar</h5>
+                <p class="card-text fs-5">Per chi Vuole solo il meglio!</p>
+                <a class="btn text-light" href="">Scopri di più</a>
+            </div>
+        </div>
+    </section>
+    {{-- /SECTION II --}}
+
+
 </x-layout>
