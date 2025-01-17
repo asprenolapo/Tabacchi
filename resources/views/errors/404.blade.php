ac46ab1c0405ff4cff1b841d7f657434a3f8dbab
asprenolapo <aslape.lapo@gmail.com>
2025-01-17 16:53:51 +0100

    page 404 aggiunta manca controllo età

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="shortcut icon" href="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/11357.png" type="">
+    <title>Document</title>
+</head>
+
+<body class="bg-body-secondary">
+    {{-- HEADER --}}
+    <header class="container-fluid p-0 overflow-hidden">
+        <div class="card text-bg-dark overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1724436281331-68ae2a523d22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                class="card-img header-img" alt="..." style="height: 250px">
+            <div class="card-img-overlay d-flex flex-column justify-content-center">
+                <h1>Tabaccheria 195</h1>
+            </div>
+        </div>
+    </header>
+    {{-- /HEADER --}}
+    <!-- SECTION -->
+    <section class="row justify-content-center my-5">
+        <div class="col-6 shadow-xl bg-white text-center p-5">
+            <h1 class="text-center">OOOOOPS! "Qualcosa è andato storto!"</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <img src="/storage/asset/404.png" alt="Error 404 png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+        </div>
+    </section>
+    <!-- /SECTION -->
+</body>
+
+</html>
