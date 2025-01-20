a55457d5140d008cdfc2fa5de111de67a7f4cd21
asprenolapo <aslape.lapo@gmail.com>
2025-01-20 16:07:30 +0100

    modificato il footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,35 +1,6 @@
 <!-- FOOTER -->
-<footer
-    class="text-center text-lg-start bg-body-tertiary text-muted {{ Route::currentRouteName() == 'admin' ? 'd-none' : '' }}">
-    <!-- SECTION:SOCIAL MEDIA -->
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        {{-- <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-        </div>
-        <div>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-github"></i>
-            </a>
-        </div> --}}
-    </section>
-    <!-- /SECTION:SOCIAL MEDIA -->
-
-    <!-- SECTION:LINKS  -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted {{ Route::currentRouteName() == 'admin' ? 'd-none' : '' }}">
+    <!-- SECTION:LINKS -->
     <section class="">
         <div class="container text-center text-md-start mt-5">
             <div class="row mt-3">
@@ -37,63 +8,25 @@
                     <h6 class="text-uppercase fw-bold mb-4">
                         Tabaccheria 195, Bari
                     </h6>
-                    <p>
-                        Al vostro servizio dal 1985
-                    </p>
+                    <p>Al vostro servizio dal 1985</p>
                 </div>
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Products
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Laravel</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">LiveWire</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Javascript</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Bootstrap</a>
-                    </p>
-                </div>
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Useful links
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Pricing</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Settings</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Orders</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Help</a>
-                    </p>
-                </div>
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                    <p> <i class="fas fa-home me-3"></i> Via Giovanni Amendola, 145/E </p>
-                    <p><i class="fa-solid fa-location-pin me-3"></i> Bari, BA 70126, IT </p>
-                    <p>
-                        <i class="fas fa-envelope me-3"></i>
-                        info@example.com
-                    </p>
+                    <p><i class="fas fa-home me-3"></i> Via Giovanni Amendola, 145/E</p>
+                    <p><i class="fa-solid fa-location-pin me-3"></i> Bari, BA 70126, IT</p>
+                    <p><i class="fas fa-envelope me-3"></i><a href="mailto:leonardoferrieri@gmail.com" class="text-reset">leonardoferrieri@gmail.com</a></p>
                     <p><i class="fas fa-phone me-3"></i> + 39 080 554 11 70</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
                 </div>
             </div>
         </div>
     </section>
-    <!-- /SECTION:LINKS  -->
+    <!-- /SECTION:LINKS -->
+
+    <!-- COPYRIGHT -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2025 Copyright:AsprenoStack
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">info@example.com</a>
+        © 2025 Copyright: Leonardo Ferrieri - Tabaccheria 195.
+
     </div>
+    <!-- /COPYRIGHT -->
 </footer>
 <!-- /FOOTER -->
